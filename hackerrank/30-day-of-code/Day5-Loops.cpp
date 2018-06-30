78c08c02829616f077dba507412ccd4f4ec1a59a
Camilo J. Mezu <camilo272001@gmail.com>
2018-06-29 22:36:48 -0500

    Beauty for everyone <3

--- hackerrank/30-day-of-code/Day5-Loops.cpp
+++ hackerrank/30-day-of-code/Day5-Loops.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    for (size_t i = 1; i <= 10; i++) {
-      cout << n << " x " << i << " = " << n*i << '\n';
-    }
+        for (size_t i = 1; i <= 10; i++) {
+                cout << n << " x " << i << " = " << n*i << '\n';
+        }
 
-  return 0;
+        return 0;
 }
