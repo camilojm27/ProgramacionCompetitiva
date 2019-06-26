469fd956ade9130dcaab29a9b3a86c036d7d607a
Camilo José Mezú Mina <camilo272001@gmail.com>
2019-06-26 09:06:09 -0500

    Almost done contactos
    Contactos its in the final line and i delete some files from other branch

--- Maratones/Icesi-2019-1-Turing/D-Contactos/contactos.cpp
+++ Maratones/Icesi-2019-1-Turing/D-Contactos/contactos.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-struct Contactos
-{
+struct Contactos {
 
     vector<string> numeros;
     vector<string> correos;
@@ -14,75 +13,82 @@ struct Contactos
     Contactos *derecha;
     Contactos *izquierda;
 
-    Contactos(string nombreU = "", string numero = "", string correo = "")
-    {
+    Contactos(string nombreU = "", string numero = "", string correo = "") {
         nombre = nombreU;
         numeros.push_back(numero);
         correos.push_back(correo);
         derecha = NULL;
         izquierda = NULL;
     }
-   
+
 };
 
-void insertContacto(Contactos *&libreta, string newNombre, string newNumero, string newCorreo)
-{
-    if (libreta == NULL)
-    {
+void insertContacto(Contactos *&libreta, string newNombre, string newNumero, string newCorreo) {
+    if (libreta == NULL) {
         libreta = new Contactos(newNombre, newNumero, newCorreo);
         return;
-    }
-    else if (newNombre == libreta->nombre)
-    {
+    } else if (newNombre == libreta->nombre) {
         libreta->numeros.push_back(newNumero);
         libreta->correos.push_back(newCorreo);
         return;
-    }
-    else if (newNombre < libreta->nombre)
-    {
+    } else if (newNombre < libreta->nombre) {
         insertContacto(libreta->izquierda, newNombre, newNumero, newCorreo);
-    }
-    else
-    {
+    } else {
         insertContacto(libreta->derecha, newNombre, newNumero, newCorreo);
     }
 }
 
-void printInfo(Contactos *libreta){
-    if (libreta != NULL){
+void printInfo(Contactos *libreta) {
+    if (libreta != NULL) {
         printInfo(libreta->izquierda);
-        cout << libreta->nombre << "\nTelefono:";
-        for(auto it = libreta->numeros.begin(); it != libreta->numeros.end(); it++){
-            cout << *it<<"\n";
+        cout << libreta->nombre << "\nTelefono:\n";
+
+        for (auto it = libreta->numeros.begin(); it != libreta->numeros.end(); it++) {
+            if(*it != ""){
+                cout << *it << "\n";
+            }
         }
-        cout << "E-Mail" << "\n";
-        for (auto it = libreta->correos.begin(); it != libreta->correos.end(); it++)
-        {
-            cout << *it << "\n";
+
+
+        cout << "E-Mail:\n";
+
+        for (auto it = libreta->correos.begin(); it != libreta->correos.end(); it++) {
+            if(*it != ""){
+                cout << *it << "\n";
+            }
+
         }
+
         cout << "###\n";
-    }    
+        printInfo(libreta->derecha);
+    }
 
 }
 
-int main()
-{
+int main() {
     int done;
     int stack;
-    do
-    {
-        while (done--)
-        {
-            
+    string nombre, apellido, dato, vacio = "";
+    cin >> done;
+    do {
+        Contactos *libreta;
+        libreta = NULL;
+        while (done--) {
+            cin >> nombre >> apellido >> dato;
+            nombre += " " + apellido;
+            if (dato.find('@') != std::string::npos) {
+                insertContacto(libreta, nombre, vacio, dato);
+            } else {
+                insertContacto(libreta, nombre, dato, vacio);
+            }
+
         }
-        
+        printInfo(libreta);
+
         cin >> done;
 
-    } while (done>0);
-    
-   
-    
-    
-    
+    } while (done > 0);
+
+
     return 0;
 }
