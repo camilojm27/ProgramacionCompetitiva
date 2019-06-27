75de4b4a542dbb1e934978a80bad6fa75395b383
Camilo José Mezú Mina <camilo272001@gmail.com>
2019-06-26 23:43:39 -0500

    Update contactos.cpp

--- Maratones/Icesi-2019-1-Turing/D-Contactos/contactos.cpp
+++ Maratones/Icesi-2019-1-Turing/D-Contactos/contactos.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <ios>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,6 +32,7 @@ void insertContacto(Contactos *&libreta, string newNombre, string newNumero, str
     } else if (newNombre == libreta->nombre) {
         libreta->numeros.push_back(newNumero);
         libreta->correos.push_back(newCorreo);
+
         return;
     } else if (newNombre < libreta->nombre) {
         insertContacto(libreta->izquierda, newNombre, newNumero, newCorreo);
@@ -71,7 +73,8 @@ int main() {
     int done,contadorLibreta=1;
     string nombre, apellido, dato, vacio = "";
     cin >> done;
-    do {
+    while (done != 0) {
+
         Contactos *libreta;
         libreta = NULL;
         while (done--) {
@@ -82,13 +85,18 @@ int main() {
             } else {
                 dato.insert(0,"(");
                 dato.insert(4, ")");
-                dato.insert(8, "-");
+                dato.insert(5, " ");
+                dato.insert(9, "-");
 
                 insertContacto(libreta, nombre, dato, vacio);
             }
 
         }
+        sort(libreta->numeros.begin(), libreta->numeros.end());
+        sort(libreta->correos.begin(), libreta->correos.end());
+
         cin >> done;
+        if (contadorLibreta > 1 and done != 0){cout << endl;}
         cout << "Lista de contactos #"<<contadorLibreta<<":";
         printInfo(libreta);
         contadorLibreta++;
@@ -96,7 +104,7 @@ int main() {
             cout << endl;
         }
 
-    } while (done > 0);
+    }
 
 
     return 0;
