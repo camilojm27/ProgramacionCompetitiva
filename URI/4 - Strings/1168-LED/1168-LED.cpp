eb2934d45690330be4bae385ef473745a10943ce
Camilo José Mezú Mina <camilo272001@gmail.com>
2019-07-01 09:34:04 -0500

    Solucionado Led - URI - Strings

--- URI/4 - Strings/1168-LED/1168-LED.cpp
+++ URI/4 - Strings/1168-LED/1168-LED.cpp
@@ -7,7 +7,7 @@ int main()
     ios_base::sync_with_stdio(false);
     int stack;
     int totalLeds;//0  1  2  3  4  5  6  7  8  9
-    int leds[10] = {6, 2, 5, 4, 4, 5, 6, 3, 7, 6};
+    string leds[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     string entrada;
     cin >> stack;
     while (stack--)
@@ -16,21 +16,52 @@ int main()
         for (int led = 0; led < 10; led++)
         {
             
-            auto found = entrada.find_first_of(leds[led]);
+            size_t found = entrada.find(leds[led]);
             while (found != string::npos)
             {
-                howMany[led] +=leds[led];
-                found++;
+                howMany[led] += 1 ;
+                found = entrada.find(leds[led], found + 1);
             }
             
 
         }
         for (int i = 0; i < 10; i++)
         {
-            suma += howMany[i];
+            switch (i) {
+                case 0:
+                    suma += howMany[i] * 6;
+                    break;
+                case 1:
+                    suma += howMany[i] * 2;
+                    break;
+                case 2:
+                    suma += howMany[i] * 5;
+                    break;
+                case 3:
+                    suma += howMany[i] * 5;
+                    break;
+                case 4:
+                    suma += howMany[i] * 4;
+                    break;
+                case 5:
+                    suma += howMany[i] * 5;
+                    break;
+                case 6:
+                    suma += howMany[i] * 6;
+                    break;
+                case 7:
+                    suma += howMany[i] * 3;
+                    break;
+                case 8:
+                    suma += howMany[i] * 7;
+                    break;
+                case 9:
+                    suma += howMany[i] * 6;
+                    break;
+            }
         }
         
-        cout << suma << " " << "leds";
+        cout << suma << " " << "leds" << endl;
         
     }
 
