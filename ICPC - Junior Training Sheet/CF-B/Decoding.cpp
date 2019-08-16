918a2237487509aef9ffe6a65e56d949e5a9de3d
camilojm27 <camilo272001@gmail.com>
2019-08-16 18:11:14 -0500

    Create Decoding.cpp

---
+++ ICPC - Junior Training Sheet/CF-B/Decoding.cpp
@@ -0,0 +1,35 @@
+//http://codeforces.com/contest/746/problem/B
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cstdio>
+
+using  namespace std;
+int main() {
+
+    //freopen("out.txt", "w", stdout);
+    int lenght;
+    string word,  out="";
+    char character;
+    cin >> lenght >> word;
+
+    while (lenght != 0){
+        if (lenght%2){//Si da 1 es impar y entra
+            character = word.at(ceil(lenght/2));
+            word.erase(word.begin() + ceil(lenght/2));
+        }
+        else{
+            character =  word.at(lenght/2);
+            word.erase(word.begin() + lenght/2);
+        }
+        //out += character;
+
+        lenght--;
+    }
+
+    for (int i = word.size(); i > 0 ; --i) {
+        
+    }
+    cout << out;
+    return 0;
+}
