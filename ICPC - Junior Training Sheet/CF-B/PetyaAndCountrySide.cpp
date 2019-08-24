b1a0d040dd3227b2a224fdc269e20e8edc844417
camilojm27 <camilo272001@gmail.com>
2019-08-24 17:08:41 -0500

    Almost done

--- ICPC - Junior Training Sheet/CF-B/PetyaAndCountrySide.cpp
+++ ICPC - Junior Training Sheet/CF-B/PetyaAndCountrySide.cpp
@@ -1,52 +1,71 @@
 //http://codeforces.com/contest/66/problem/B
 //2.30 +
 //6.37
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
-int main()
-{
+
+int main() {
     int gardenSize;
     cin >> gardenSize;
-    int gardeHeights[gardenSize], gardenMax[gardenSize];
-
-    for (int i = 0; i < gardenSize; i++)
-    {
-        cin >> gardeHeights[i];
-        gardenMax[i] = 0;
+    int alturas[gardenSize], puntajes[gardenSize];
+    for (int i = 0; i < gardenSize; ++i) {
+        cin >> alturas[i];
+        puntajes[i] = 0;
     }
+if (gardenSize ==1){
+    cout << 1;
+} else{
+    for (int i = 0; i < gardenSize; ++i) {
+        int a=0, b=0, next=0;
 
-    for (int i = 0; i < gardenSize; i++)
-    {
-        int maximo = 0;
-        if (i <= gardenSize - 1)
-        {
-
-            int next = gardeHeights[i + 1];
-            if (gardeHeights[i] >= gardeHeights[i + 1])
-            {
-                int localI = i;
-                while (gardeHeights[i] >= gardeHeights[i + 1] and i + 1 < gardenSize)
-                {
-                    gardenMax[i]++;
-                    localI++;
-                }
-                while (i-1 >= 0 and gardeHeights[i] >= gardeHeights[i-1])
-                {
-                    gardenMax[i]++;
-                }
-                
+        next=i+1;
+        if (alturas[i] >= alturas[next]) {
+            a = alturas[i], b=alturas[next];
+            while (a >= b){
+               // cout << "entro en i =" << i<<endl;
+                puntajes[i]+=1;
+                a=alturas[next];
+                next++;
+                b=alturas[next];
             }
-            
         }
-        
-        
-        sort(gardenMax, gardenMax+1);
     }
-    for (size_t i = 0; i < gardenSize; i++)
-    {
-        cout << gardenMax[i];
+    for (int j = 0; j < gardenSize; ++j) {
+        //cout << puntajes[j];
     }
-    
+   // cout << endl;
+
+    for (int k = gardenSize - 1; k >= 0 ; --k) {
+        int a, b, next;
+
+        next=k-1;
+        if (alturas[k] >= alturas[next]) {
+            a = alturas[k], b=alturas[next];
+            while (a >= b){
+                //cout << "entro en k =" << k <<endl;
+                puntajes[k]+=1;
+                a=alturas[next];
+                next--;
+                b=alturas[next];
+            }
+        }
+    }
+
+    /*
+    cout << "n = " << n <<endl;
+    for (int j = 0; j < gardenSize; ++j) {
+        cout << puntajes[j];
+    }*/
+    sort(puntajes, puntajes+gardenSize, greater<int>());
+
+    cout << puntajes[0] + 1;
+}
+
     return 0;
 }
+/*
+ 8
+1 2 1 1 1 3 3 4
+*/
