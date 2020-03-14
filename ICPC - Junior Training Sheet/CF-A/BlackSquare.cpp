619538ffdbee0d23ac2fc64139d0cc6341019813
Camilo Mezú Mina <camilo272001@gmail.com>
2020-03-14 16:20:09 -0500

    Update BlackSquare.cpp

--- ICPC - Junior Training Sheet/CF-A/BlackSquare.cpp
+++ ICPC - Junior Training Sheet/CF-A/BlackSquare.cpp
@@ -48,3 +48,32 @@ int main()
 
     return 0;
 }
+/*Another Way
+
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main()
+    {
+    string s;
+    int a1, a2, a3, a4, ans;
+    while (cin >> a1 >> a2 >> a3 >> a4)
+    {
+    ans = 0;
+    cin >> s;
+    int l = s.size();
+    for (int i = 0; i < l; ++i) 
+    {
+    int val = s[i] - '0';
+    if (val == 1) ans += a1;
+    else if (val == 2) ans += a2;
+    else if (val == 3) ans += a3;
+    else ans += a4;
+    }
+    cout << ans << endl;
+    }
+     
+        return 0;
+    }
+*/
